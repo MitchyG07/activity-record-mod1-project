0e6b58bd68b0884416105802d35874f83b78d709
emmafewer <emmafewer20@gmail.com>
2020-12-02 21:03:28 -0800

    cleaned up find_by_state #2

--- app/models/nationalpark.rb
+++ app/models/nationalpark.rb
@@ -3,14 +3,12 @@ class Nationalpark < ActiveRecord::Base
     has_many :tourists, through: :permits 
 
     def self.find_by_state(state)
-        array = []
-        counter = 0
         state = GetRequester.new("https://developer.nps.gov/api/v1/parks?stateCode=#{state}&api_key=amSoAnSXu48zw8sNc45tsop4Wriei2yPYPM8hrBJ")
         state_data = state.parse_json
         state_data["data"].map{|np| np["fullName"]}
     end 
 end 
-       
+       #I replaced line 8 with the below:
         # array = []
         # counter = 0
         # while counter < state_data["data"].length
