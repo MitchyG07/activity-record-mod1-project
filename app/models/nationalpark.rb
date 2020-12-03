14aa0c342fe6ebc07f3d643e6139b325d74cace0
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-02 18:11:00 -0800

    creates a find_by_state class method

--- app/models/nationalpark.rb
+++ app/models/nationalpark.rb
@@ -2,7 +2,7 @@ class Nationalpark < ActiveRecord::Base
     has_many :permits
     has_many :tourists, through: :permits 
 
-    def find_by_state(state)
+    def self.find_by_state(state)
         array = []
         counter = 0
         state = GetRequester.new("https://developer.nps.gov/api/v1/parks?stateCode=#{state}&api_key=amSoAnSXu48zw8sNc45tsop4Wriei2yPYPM8hrBJ")
