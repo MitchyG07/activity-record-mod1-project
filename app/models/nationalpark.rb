63fc19cc512607d6d2f8fb608cc50ce30986c70a
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-02 17:26:09 -0800

    working on find_by_state method

--- app/models/nationalpark.rb
+++ app/models/nationalpark.rb
@@ -1,4 +1,17 @@
 class Nationalpark < ActiveRecord::Base
     has_many :permits
     has_many :tourists, through: :permits 
+
+    def find_by_state(state)
+        array = []
+        state = GetRequester.new("https://developer.nps.gov/api/v1/parks?stateCode=#{state}&api_key=amSoAnSXu48zw8sNc45tsop4Wriei2yPYPM8hrBJ")
+        state_data = state.parse_json
+        state_data['data'].each do |np|
+            np.each do |p|
+                array = p["fullName"]
+            end
+        end 
+        puts array
+    end 
+       
 end 
