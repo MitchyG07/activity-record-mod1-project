658cf63f8fdeb4e7c1df1f422e80fb61ae0551ca
Mitch Gilbert <mitchellpgilbert@gmail.com>
2020-12-02 18:04:55 -0800

    returns all NPs in a given state

--- app/models/nationalpark.rb
+++ app/models/nationalpark.rb
@@ -4,14 +4,14 @@ class Nationalpark < ActiveRecord::Base
 
     def find_by_state(state)
         array = []
+        counter = 0
         state = GetRequester.new("https://developer.nps.gov/api/v1/parks?stateCode=#{state}&api_key=amSoAnSXu48zw8sNc45tsop4Wriei2yPYPM8hrBJ")
         state_data = state.parse_json
-        state_data['data'].each do |np|
-            np.each do |p|
-                array = p["fullName"]
-            end
+        while counter < state_data["data"].length
+            array << state_data["data"][counter]["fullName"]
+            counter += 1 
         end 
         puts array
     end 
-       
 end 
+       
